2564a1d3efdf240a952af76b03809bac59696697
HyeongJun, Kim <khj029956@naver.com>
2024-10-02 21:37:41 +0900

    Style: Prettier 사용

--- masterbook/src/App.js
+++ masterbook/src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Main from './components/home/Main';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./components/home/Main";
 // import OtherComponent from './components/OtherComponent';
 
 function App() {
