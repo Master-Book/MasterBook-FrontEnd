f3a438c534a3e8067f5b4861637e0454723321db
HyeongJun, Kim <khj029956@naver.com>
2024-10-02 21:14:14 +0900

    Doc: 프로젝트 시작
    디렉터리 구조 조정 및 임시 파일 생성

---
+++ masterbook/src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './components/home/Main';
+// import OtherComponent from './components/OtherComponent';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        {/* <Route path="/other" element={<OtherComponent />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
