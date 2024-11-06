01eb601e42f4d95ce44926ecf682c4d57ffe3b95
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2024-11-06 16:18:37 +0900

    login,signup

--- masterbook/src/components/login/Main.jsx
+++ masterbook/src/components/login/Main.jsx
@@ -2,25 +2,33 @@
 
 import React from "react";
 import "./login.css";
+import { Link } from "react-router-dom";
 
 function Main() {
   return (
-    <div className="login-container">
+    <div className="outer-container">
       <div className="avatar">
         <i className="fa fa-user"></i>
       </div>
+    <div className="login-container">
+      
       <h2>Login</h2>
       <form>
         <div className="input-container">
           <i className="fa fa-envelope icon"></i>
-          <input type="text" placeholder="Email ID" required />
+          <input className="login_input" type="text" placeholder="Email ID" required />
         </div>
         <div className="input-container">
           <i className="fa fa-lock icon"></i>
-          <input type="password" placeholder="Password" required />
+          <input className="login_input" type="password" placeholder="Password" required />
         </div>
         <button type="submit" className="login-button">Login</button>
       </form>
+      <div className="signup-link">
+        <p className="signup-p">계정이 없으신가요?</p>
+        <Link to="/signup" className="login-button">회원가입</Link>
+      </div>
+    </div>
     </div>
   );
 }
