63fd2f7535c9581752fdf7d7b21da77971de96ec
HyeongJun, Kim <khj029956@naver.com>
2024-11-06 09:44:54 +0900

    CSS: Main 캐릭터 CSS 변경

--- masterbook/src/components/channel/Main.jsx
+++ masterbook/src/components/channel/Main.jsx
@@ -1,3 +1,5 @@
+// src/components/channel/Main.jsx
+
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import InitialFilter from "./InitialFilter";
