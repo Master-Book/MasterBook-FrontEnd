36d917da5a6641e960e58f274719859f63fd66fe
HyeongJun Kim <khj029956@naver.com>
2024-11-11 07:15:14 +0900

    CSS: 홈페이지에 최신 글 6개 보기 추가

--- masterbook/src/components/home/Main.jsx
+++ masterbook/src/components/home/Main.jsx
@@ -1,15 +1,71 @@
 // src/components/home/Main.js
 
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import './Main.css'; // 스타일링을 위한 CSS 파일
 
 function Main() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // 더미 데이터(posts.json)에서 게시글 불러오기
+    axios
+      .get('/posts.json')
+      .then((response) => {
+        // // 게시글을 날짜 순으로 정렬 (최신순)
+        // const sortedPosts = response.data.sort(
+        //   (a, b) => new Date(b.date) - new Date(a.date)
+        // );
+
+        // 게시글을 포스트 ID 높은 순으로 정렬 -> 나중에 날짜 순으로 변경 해야 함.
+        const sortedPosts = response.data.sort((a, b) => b.id - a.id);
+
+        // 최신 게시글 6개만 선택
+        const latestPosts = sortedPosts.slice(0, 6);
+        setPosts(latestPosts);
+      })
+      .catch((error) => {
+        console.error('Error fetching posts:', error);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Masterbook에 오신 것을 환영합니다!</h1>
-      <p>원하는 게임을 선택하여 공략 정보를 확인하세요.</p>
+    <div className="main-container">
+      {/* <h1>Masterbook에 오신 것을 환영합니다!</h1> */}
+      {/* <p>원하는 게임을 선택하여 공략 정보를 확인하세요.</p> */}
+      <p>최신 글</p>
+      <div className="post-card-list">
+        {posts.map((post) => (
+          <div key={post.id} className="post-card">
+            <div className="post-card-header">
+              <span className="game-id">{post.gameId}</span>|
+              <span className="character-id">{post.characterId}</span>
+              {/* 게임 로고 이미지 추가 */}
+              <img
+                src={`/images/${post.gameId}/logo/logo.png`}
+                alt={`${post.gameId} Logo`}
+                className="game-logo"
+              />
+            </div>
+            <h2 className="post-title">{post.title}</h2>
+            <p className="post-author">작성자: {post.author}</p>
+            <p className="post-date">작성일: {post.date}</p>
+            <div className="post-content">{post.content}</div>
+            <button className="read-more-button">
+              <Link
+                to={`/${post.gameId}/${post.characterId}/${post.id}`}
+                className="read-more-link"
+              >
+                더 보기
+              </Link>
+            </button>
+          </div>
+        ))}
+      </div>
+      {/* 글 작성 페이지로 이동하는 링크 */}
       <li>
-        <Link to="/postWrite">글작성</Link>
+        <Link to="/postWrite">글 작성</Link>
       </li>
     </div>
   );
