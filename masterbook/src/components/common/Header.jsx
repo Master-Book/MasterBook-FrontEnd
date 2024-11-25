9906e01b4167d495ae8541aee703a5c615d1b7c1
HyeongJun Kim <khj029956@naver.com>
2024-11-25 23:56:11 +0900

    Feat: 최신 게시글 표시

--- masterbook/src/components/common/Header.jsx
+++ masterbook/src/components/common/Header.jsx
@@ -1,10 +1,41 @@
 // src/components/common/Header.js
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './Header.css';
 
+const SERVER_IP = process.env.REACT_APP_SERVER_IP;
+
 function Header() {
+  const navigate = useNavigate();
+  const isLoggedIn = !!localStorage.getItem('token');
+  console.log('token:', localStorage.getItem('token'));
+  console.log('isLoggedIn:', isLoggedIn);
+
+  const handleLogout = () => {
+    // '/logout' 엔드포인트로 로그아웃 요청 보내기
+    axios
+      .post(
+        `${SERVER_IP}/logout`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }
+      )
+      .then(() => {
+        // 로그아웃 성공 시 토큰 삭제 및 페이지 이동
+        localStorage.removeItem('token');
+        navigate('/');
+      })
+      .catch((error) => {
+        console.error('로그아웃 중 에러 발생:', error);
+        // 에러 처리 (필요에 따라 사용자에게 에러 메시지 표시)
+      });
+  };
+
   return (
     <header className="header">
       <div className="header-content">
@@ -12,7 +43,7 @@ function Header() {
           <Link to="/">
             <img
               src={`/images/master_book/logo_white.png`}
-              alt={`logo`}
+              alt="logo"
               className="logo"
             />
             Master Book
@@ -22,7 +53,13 @@ function Header() {
           {/* 문의하기 링크 추가 */}
           <Link to="/contact">문의하기</Link>
           <span className="separator">|</span>
-          <Link to="/login">로그인/회원가입</Link>
+          {isLoggedIn ? (
+            <button onClick={handleLogout} className="logout-button">
+              로그아웃
+            </button>
+          ) : (
+            <Link to="/login">로그인/회원가입</Link>
+          )}
         </div>
       </div>
       <nav className="subtitle">
@@ -30,7 +67,7 @@ function Header() {
           <ul>
             <li>
               <img
-                src="images/league_of_legends/logo/logo.png"
+                src="/images/league_of_legends/logo/logo.png"
                 width="20"
                 alt="LoL logo"
               />
@@ -39,7 +76,7 @@ function Header() {
             |
             <li>
               <img
-                src="images/stardew_valley/logo/logo.png"
+                src="/images/stardew_valley/logo/logo.png"
                 width="20"
                 alt="Stardew Valley logo"
               />
